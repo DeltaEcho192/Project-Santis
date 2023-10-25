0fff22ef2db504908cc9f1b6f58825e50766e3d7
DeltaEcho192 <anthony@durrer.co.za>
2023-10-25 21:58:48 +0000

    Working form and database insert

---
+++ insert.sql
@@ -0,0 +1,1 @@
+INSERT INTO items (item_id, item_name, size, weight, value, packed, category, sub_category) VALUES $1, $2, $3, $4, $5, $6, $7, $8;
--- santis/server.js
+++ santis/server.js
@@ -3,6 +3,7 @@ const pug = require('pug');
 const bodyParser = require('body-parser');
 const path = require('path');
 const { v4 : uuid } = require('uuid');
+const sqlite3 = require('sqlite3');
 
 const PORT = process.env.PORT || 2502
 
@@ -12,15 +13,50 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'assets')))
 
+let db = new sqlite3.Database("santis.db" , (err) => { 
+    if (err) { 
+        console.log("Error Occurred - " + err.message); 
+    } else { 
+        console.log("DataBase Connected"); 
+    } 
+})
+
 app.get('/', (req, res) => {
-	res.render('index')
+	values = ["test1", "test2"]
+	res.render('index', {cats: values})
 });
 
 app.post('/test', (req, res) => {
-	const { test } = req.body;
-	console.log(test)
-	res.send(`<p>${test}</p>`)
+	console.log(req.body.item_name)
+	let id = uuid();
+	console.log(id)
+	let r = req.body;
+	if (r.packed == "on") {
+		r.packed = true;
+	} else {
+		r.packed = false;
+	}
+	console.log(r.packed)
+	try {
+	db.run("INSERT INTO items ('item_id', 'item_name', 'size', 'weight', 'value', 'packed', 'category', 'sub_category') VALUES ($id, $item_name, $size, $weight, $value, $packed, $category, $sub_category);", 
+		{$id:id,
+			$item_name: r.item_name,
+			$size: r.size,
+			$weight: r.weight,
+			$value: r.value,
+			$packed: r.packed,
+			$category: r.category,
+			$sub_category: r.sub_category});
+
+	} catch (e) {
+		res.send("<p>Error</p>")
+	}
+	res.send("<p>Success</p>")
 });
+
+app.post('/save', (req, res) => {
+	
+})
 
 app.listen(PORT);
 console.log("Santis is listening on port: " + PORT);
