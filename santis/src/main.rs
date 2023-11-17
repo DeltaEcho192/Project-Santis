acd0799701c7c687571bcb31a5f3a64a2aa85ff8
DeltaEcho192 <anthony@durrer.co.za>
2023-11-17 10:44:39 +0200

    Created test endpoint with sqlite connection
    Added example on how to use sqllite with the endpoints

--- santis/src/main.rs
+++ santis/src/main.rs
@@ -4,6 +4,7 @@ use axum::{
     http::{StatusCode, HeaderMap},
     response::IntoResponse,
     Json, Router, extract::Path,
+    extract::State
     
 };
 use tower::ServiceExt;
@@ -14,19 +15,28 @@ use tower_http::{
 mod template_struct;
 use template_struct::*;
 use uuid::Uuid;
+use sqlx::{sqlite::SqliteQueryResult, Sqlite, SqlitePool, Row};
+
+#[derive(Clone)]
+struct Appstate {
+    pool: SqlitePool
+}
 
 #[tokio::main]
 async fn main() {
     // initialize tracing
     tracing_subscriber::fmt::init();
-
+    let pool = SqlitePool::connect("santis.db").await.unwrap();
+    let app = Appstate {pool: pool};
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(root))
         .route("/list", get(list))
         .route("/items/:id/edit", get(row_edit))
-        .nest_service("/assets", ServeDir::new("assets"));
+        .route("/test", get(add_item))
+        .nest_service("/assets", ServeDir::new("assets"))
+        .with_state(app);
 
     // run our app with hyper, listening globally on port 3000
     axum::Server::bind(&"0.0.0.0:2502".parse().unwrap())
@@ -45,6 +55,17 @@ async fn root() -> impl IntoResponse {
     let mut headers = HeaderMap::new();
     headers.insert("Content-Type", "text/html; charseet=utf-8".parse().unwrap());
     (headers, render)
+}
+
+async fn add_item(State(state): State<Appstate>) -> impl IntoResponse {
+    let item_id = Uuid::new_v4();
+    println!("New item id: {}", item_id);
+    let sql_query = "Select * FROM contacts;";
+    let result = sqlx::query(sql_query).fetch_all(&state.pool).await.unwrap();
+    for (idx, row) in result.iter().enumerate() {
+        println!("[{}]: {:?}", idx, row.get::<String, &str>("first_name"));
+    }
+    
 }
 
 async fn list() -> impl IntoResponse {
